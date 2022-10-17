dc03b45923f5ad0a83e161b9dd9f573c9f71ab8a
Igor Cardoso <igorgcardoso7@gmail.com>
2022-10-17 10:58:03 -0300

    Take out userAction

--- src/Pages/SignIn.stories.tsx
+++ src/Pages/SignIn.stories.tsx
@@ -1,21 +1,11 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { userEvent, within } from '@storybook/testing-library';
-import SignIn from './SignIn';
+import { Meta, StoryObj } from "@storybook/react";
+import SignIn from "./SignIn";
 
 export default {
-  title: 'Pages/Sign in',
+  title: "Pages/Sign in",
   component: SignIn,
   args: {},
-  argTypes: {}
+  argTypes: {},
 } as Meta;
 
-export const Default: StoryObj = {
-  play: ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-
-    userEvent.type(canvas.getByPlaceholderText("Digite seu e-mail"), 'igorgcardoso7@gmail.com');
-    userEvent.type(canvas.getByPlaceholderText("********"), '123456789');
-
-    userEvent.click(canvas.getByRole('button'));
-  }
-};
+export const Default: StoryObj = {};
